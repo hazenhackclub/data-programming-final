1213d25b6f31ae06eff508b522befe44cdae2cc2
Sam DePoule <samdepoule@gmail.com>
2023-06-07 12:47:00 -0700

    Succsessfully merged all data

--- src/main.py
+++ src/main.py
@@ -12,23 +12,22 @@ def import_map(filename):
 	return gpd.read_file(filename)
 
 
-def create_graph(map, name="Plot"):
+def create_graph(map, name="Plot", data="POPESTIMATE2018"):
 	"""
-	legend=True, column='margin', cmap='Crimes Per Thousand'
+	
 	"""
 
 	fig, ax = plt.subplots(1)
 
-	figsize = (6.4, 4.8)
-	s = 2
 	map.plot(ax=ax, color='#bababa')
-	map.plot(ax=ax, column='POPESTIMATE2018', legend=True, linewidth=10, figsize=(s * figsize[0], s * figsize[1]))
+	map.plot(ax=ax, column=data, legend=True, linewidth=10)
 	
 	plt.xlabel('This is the longitude')
 	plt.ylabel('This is the latitude')
 	plt.title("This is a graph with resolution of 400 dpi")
-	plt.axis([-128, -64, 22, 52])
-	plt.savefig(name + '.png', dpi=800)
+	# plt.axis([-128, -64, 22, 52])
+	plt.axis([-182, -59, 14, 74])  # other territories included
+	plt.savefig(name + '.png', dpi=700)
 
 
 def convert_columns_to_floats(data, columns=[]):
@@ -38,9 +37,10 @@ def convert_columns_to_floats(data, columns=[]):
 
 def main():
 	print('Importing Data')
-	washington_map = import_map('./src/washington.json')
+	# washington_map = import_map('./src/data/washington.json')
 	county_map = import_map('./src/data/5m-US-counties.json')
 	crime_data = import_data('./src/data/US_Offense_Type_by_Agency_2018-trim.csv')
+	fips_codes = import_data('./src/data/statefipscodes.csv')
 	pop_data = pandas.read_csv('./src/data/popest2018-trim.csv', encoding='latin-1')
 
 	# Regularize the data
@@ -48,36 +48,30 @@ def main():
 	agency_type_county = crime_data['Agency Type'].str.lower().str.contains("count")
 	crime_data = crime_data[agency_type_county]
 
-	# pop_data['CTYNAME'] = pop_data['CTYNAME'].astype(str)
-	# print('as string', pop_data, '\n\n\n')
 	county_county = pop_data['CTYNAME'].str.lower().str.contains("count")
 	pop_data = pop_data[county_county]
-	# print('remove cities', county_county, '\n\n\n')
 	pop_data['CTYNAME'] = pop_data['CTYNAME'].str.replace(' County', '')
-	# print('remove county word', pop_data, '\n\n\n')
 
-	# print(crime_data.loc[:, 'Agency Name'])
-	# print(county_map.loc[:, 'NAME'])
-	# print(pop_data.loc[:, 'CTYNAME'])
+	county_map['STATEFP'] = county_map['STATEFP'].astype(int)
+	merged_map = county_map.merge(fips_codes, left_on='STATEFP', right_on='FIPS', how='right')
+	
+	# Combines state and county names for merging
+	crime_data['state_county'] = crime_data['State'].str.upper() + '-' + crime_data['Agency Name'].str.upper()
+	pop_data['state_county'] = pop_data['STNAME'].str.upper() + '-' + pop_data['CTYNAME'].str.upper()
+	merged_map['state_county'] = merged_map['State'].str.upper() + '-' + merged_map['NAME'].str.upper()
 
-	crime_data['test_crime'] = -1
-	pop_data['test_pop'] = 0
-	county_map['test_map'] = 1
+	# Merging data
+	print('Merging data')
+	merged_data = pop_data.merge(crime_data, left_on='state_county', right_on='state_county', how='outer')
 
-	merged_data = pop_data.merge(crime_data, left_on='CTYNAME', right_on='Agency Name', how='outer')
+	merged_final = merged_map.merge(merged_data, left_on='state_county', right_on='state_county', how='left')
 
-	pandas.set_option('display.max_rows', 500)
-	pandas.set_option('display.max_columns', 500)
-
-	# print(washington_map)
-	print('merged_data', merged_data)
-
-	merged_map = county_map.merge(merged_data, left_on='NAME', right_on='CTYNAME', how='left')
-
-	print('merged_map', merged_map)
+	# pandas.set_option('display.max_rows', 500)
+	# pandas.set_option('display.max_columns', 500)
+	# print('merged_final', merged_final)
 
 	# Find the number of crimes per 1,000 people by coutny
-	# merged['Crimes Per Thousand'] = 1000 * crime_data['Total Offenses'] / crime_data['Population1']
+	merged_final['Crimes_per_thousand'] = 1000 * merged_final['Total Offenses'] / merged_final['POPESTIMATE2018']
 
 	"""
 	county_map columns: ['id', 'STATEFP', 'COUNTYFP', 'COUNTYNS', 'AFFGEOID', 'GEOID', 'NAME', 'LSAD', 'ALAND', 'AWATER', 'geometry']
@@ -99,7 +93,8 @@ def main():
 	ARIZONA Crimes Per Thousand: 47.934547
 	"""
 	
-	# create_graph(merged_map)
+	print('Graphing data')
+	create_graph(merged_final, data='STATEFP')
 	
 	print('Finished.')
 
